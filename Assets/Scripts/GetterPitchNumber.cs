6230a58a754d198c3d2f92ad032c2df12bb04ca1
woods-one <0820ricer@gmail.com>
2022-03-03 16:34:22 +0900

    追加 : コメントアウトの追加

--- Assets/Scripts/GetterPitchNumber.cs
+++ Assets/Scripts/GetterPitchNumber.cs
@@ -2,20 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ピッチの高さを取得するクラス
+/// </summary>
 public class GetterPitchNumber : MonoBehaviour
 {
     private int maxIndex = 0;
-    private int freq;
+    private int freq;// 周波数
     private int pitchNumber;
     private float maxValue = 0.0f;
+
+    [SerializeField]
+    private AudioSource aud;
+
     void Start()
     {
-        AudioSource aud = GetComponent<AudioSource>();
         // マイク名、ループするかどうか、AudioClipの秒数、サンプリングレート を指定する
         aud.clip = Microphone.Start(null, true, 10, 44100);
         aud.Play();
     }
-    void Update()
+    void FixedUpdate()
+    {
+        GetPitchNumber();
+    }
+
+    /// <summary>
+    /// ピッチの高さを数字として取得する
+    /// </summary>
+    private void GetPitchNumber()
     {
         float[] spectrum = new float[8192];
         AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
@@ -34,7 +48,10 @@ public class GetterPitchNumber : MonoBehaviour
         pitchNumber = calculateNoteNumberFromFrequency(freq);
     }
 
-    // See https://en.wikipedia.org/wiki/MIDI_tuning_standard
+    /// <summary>
+    /// MIDI Tuning Standard数字を変換する
+    /// See https://en.wikipedia.org/wiki/MIDI_tuning_standard
+    /// </summary>
     private int calculateNoteNumberFromFrequency(float freq)
     {
         return Mathf.FloorToInt(69 + 12 * Mathf.Log(freq / 440, 2));
