51e7a8fe177fbd45c4fe0236cd0cc789cbb749a3
Takayama <823ab172@gmail.com>
2022-03-06 22:31:04 +0900

    変更:IconPanelにsummaryを追加,初期値を設定できるように変更

--- Assets/Scripts/UI/IconPanel.cs
+++ Assets/Scripts/UI/IconPanel.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class IconPanel : MonoBehaviour
 {
     [SerializeField]private GameObject IconObj;
+    [SerializeField]private int firstSetNum;
     private void Start(){
-        SetIcon(3);
+        SetIcon(firstSetNum);
     }
-    public void SetIcon(int life) {
+    
+    ///<summary>
+    ///渡した引数分のアイコンを表示します
+    ///</summary>
+    public void SetIcon(int setNum) {
         for (int i = 0; i < transform.childCount; i++) {
             Destroy(transform.GetChild(i).gameObject);
         }
-        for (int i = 0; i < life; i++) {
+        for (int i = 0; i < setNum; i++) {
             Instantiate<GameObject>(IconObj, transform);
         }
     }
