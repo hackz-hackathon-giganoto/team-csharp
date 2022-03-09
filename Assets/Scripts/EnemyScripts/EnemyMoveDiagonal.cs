31a66161439fc6e674a5e7601d064639a3ef9ae5
kaito0818 <k.riku.momo@icloud.com>
2022-03-09 22:57:52 +0900

    変更：moveSpeedをmoveSpeedXに変更　moveSpeed2をmoveSpeedYに変更

--- Assets/Scripts/EnemyScripts/EnemyMoveDiagonal.cs
+++ Assets/Scripts/EnemyScripts/EnemyMoveDiagonal.cs
@@ -1,41 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 /// <summary>
 /// 敵の斜め方向のみの移動をさせるクラス
 /// </summary>
 public class EnemyMoveDiagonal : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed;
+    private float moveSpeedX;
 
     [SerializeField]
     private bool isRight;
 
     [SerializeField]
     private bool isUp;
-
-    float moveSpeed2;
+    float moveSpeedY;
 
     void Start()
     {
-        moveSpeed2 = moveSpeed;
+        moveSpeedY = moveSpeedX;
         if (!isRight)
         {
-            moveSpeed *= -1;
+            moveSpeedX *= -1;
         }
 
         if (!isUp)
         {
-            moveSpeed2 *= -1;
+            moveSpeedY *= -1;
         }
     }
-    
+
 
     void FixedUpdate()
     {
-        Vector3 position = new Vector3(moveSpeed, moveSpeed2, 0);
+        Vector3 position = new Vector3(moveSpeedX, moveSpeedY, 0);
         transform.Translate(position);
     }
-}
+} 
