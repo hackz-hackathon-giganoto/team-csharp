a1ddaa06792758da4c314fb54350d1a6de7594b5
Takayama <823ab172@gmail.com>
2022-03-11 23:55:43 +0900

    追加:ScoreManagerクラスを作成

---
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+ public class ScoreManager : MonoBehaviour
+ {
+    public static ScoreManager instance = null;
+    [SerializeField] GameObject scoreObject;
+    private TextMeshProUGUI scoreTMP;
+    private int score = 0;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject); 
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        scoreTMP =  scoreObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void increaseScore(int changeScore)
+    {
+    score += changeScore;
+    scoreTMP.text=score.ToString();
+    }
+ }
+ 
