72a9f7d11323de83d0e87605c6e29651125bc723
Takayama <823ab172@gmail.com>
2022-03-13 14:35:51 +0900

    追加:ランキング取得メソッドを作成

--- Assets/Scripts/UI/RankingPanel.cs
+++ Assets/Scripts/UI/RankingPanel.cs
@@ -1,20 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
 
 ///<summary>
 ///ランキングを表示するクラス
 ///</summary>
 public class RankingPanel : MonoBehaviour
 {
+    private List<User> rankingUserList;
     public void  Awake(){
+        GetRanking();
+    }
 
+    private void GetRanking()
+    {
+        PlayFabClientAPI.GetLeaderboard(
+        new GetLeaderboardRequest
+        {
+            StatisticName = "HighScore", 
+            StartPosition = 0, 
+            MaxResultsCount = 100 
+        },
+        result=>
+        {
+            foreach (var user in result.Leaderboard){
+                rankingUserList.Add(new User(user.StatValue,user.DisplayName));
+                Debug.Log($"{rankingUserList.Last().UserScore}:{rankingUserList.Last().UserName}さん");
+            };
+        },
+        error=>{
+            Debug.Log($"{error.Error}");
+        }
+        );
+
+  
     }
 
     ///<summary>
     ///ユーザ名とユーザスコアを管理する内部クラス
     ///</summary>
-    private class User
+    public class User
     {
     public int UserScore{get;set;}
     public string UserName{get;set;}
