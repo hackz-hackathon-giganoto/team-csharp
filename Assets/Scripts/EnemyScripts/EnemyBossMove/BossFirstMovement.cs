3490e379c65f54aaa035ebd6587c80ec2f2de688
woods-one <0820ricer@gmail.com>
2022-03-14 01:57:27 +0900

    追加: ボスの動きの追加をしました

---
+++ Assets/Scripts/EnemyScripts/EnemyBossMove/BossFirstMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFirstMovement : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> bossMovePositions;
+
+    private int randomMoveStartNumber;
+
+    private int randomMoveEndNumber;
+
+    [SerializeField]
+    private float moveIntervalTime;
+
+    [SerializeField]
+    private float bossMoveSpeed;
+
+    [SerializeField]
+    private Rigidbody2D rigidBody;
+
+    private Vector3 enemyDirection;
+
+    private void Start()
+    {
+        StartCoroutine("hoge");
+    }
+
+    IEnumerator hoge()
+    {
+        Random.InitState(System.DateTime.Now.Millisecond);
+        randomMoveStartNumber = Random.Range(0, 12);
+
+        switch (randomMoveStartNumber/4)
+        {
+            case 0:
+                randomMoveEndNumber = Random.Range(8, 12);
+                break;
+            case 1:
+                randomMoveEndNumber = Random.Range(12, 16);
+                break;
+            case 2:
+                randomMoveEndNumber = Random.Range(0, 4);
+                break;
+        }
+        this.gameObject.transform.position = bossMovePositions[randomMoveStartNumber].transform.position;
+        enemyDirection = (bossMovePositions[randomMoveStartNumber].transform.position - bossMovePositions[randomMoveEndNumber].transform.position);
+        this.gameObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, enemyDirection);
+        yield return new WaitForSeconds(moveIntervalTime);
+        rigidBody.velocity = transform.up * bossMoveSpeed * -1;
+    }
+}
