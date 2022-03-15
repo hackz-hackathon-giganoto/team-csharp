051fcd01fbf57bf823e0f19d4f694c7ce72c7efe
kaito0818 <k.riku.momo@icloud.com>
2022-03-16 01:55:00 +0900

    追加：第一面（EnemyNormal）を追加

---
+++ Assets/Scripts/BulletDestory.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestory : MonoBehaviour
+{
+    [SerializeField] private float time;
+    float counter = 0;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time *= 60;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+
+    {
+        counter += 1;
+        if(counter == time)
+        {
+            GameObject[] objects = GameObject.FindGameObjectsWithTag("EnemyBullet");
+            foreach (GameObject bullet in objects) 
+            {
+                Destroy(bullet);
+            }
+        }
+    }
+}
