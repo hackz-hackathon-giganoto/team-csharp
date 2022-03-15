bc5d8f00ce8e029076f8ed8db004d92b618bcccf
Takayama <823ab172@gmail.com>
2022-03-15 23:45:50 +0900

    変更:Resultシーン、Resultクラスの名前が既存のものと競合したため変更

--- Assets/Scripts/SceneLoaders/ResultScene.cs
+++ Assets/Scripts/SceneLoaders/ResultScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Result : SceneBase
+public class ResultScene : SceneBase
 {
     [SerializeField] Text scoreText;
     [SerializeField] Text grazeText;
