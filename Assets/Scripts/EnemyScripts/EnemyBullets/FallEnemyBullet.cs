02880b31388d3811a4c707ea67bd071e783ecc3f
woods-one <0820ricer@gmail.com>
2022-03-20 22:33:02 +0900

    修正: FallEnemyBulletの修正

--- Assets/Scripts/EnemyScripts/EnemyBullets/FallEnemyBullet.cs
+++ Assets/Scripts/EnemyScripts/EnemyBullets/FallEnemyBullet.cs
@@ -2,25 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 落ちる弾の生成（弾を生成するだけの）クラス
+/// </summary>
 public class FallEnemyBullet : MonoBehaviour
 {
     [SerializeField]
     GameObject enemyBullet;
 
     [SerializeField]
-    private float waitTime;
+    private float waitSeconds;
 
     void Start()
     {
         StartCoroutine(WaitInstans());
     }
 
+    /// <summary>
+    /// 弾の一定期間で生成するメソッド
+    /// </summary> 
     IEnumerator WaitInstans()
     {
         while (true)
         {
             Instantiate(enemyBullet, this.gameObject.transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(waitTime);
+            yield return new WaitForSeconds(waitSeconds);
         }
     }
 }
